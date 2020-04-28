0240632a48605b15afad1366987b22eb2b24fc8d
Raull <silva.raull@gmail.com>
2020-04-28 11:38:00 -0300

    Add new cases/deaths per day

--- brazil_.R
+++ brazil_.R
@@ -2,7 +2,7 @@
 rm(list=ls())
 
 #change the directory the code
-setwd("/home/silva/R_Diretorio/sars-covid-19")
+#setwd("/home/silva/R_Diretorio/sars-covid-19")
 
 
 library(data.table)
@@ -14,16 +14,37 @@ library(tidyr)
 # extract the data from the repository from the computer
 new_cases <- fread("C:/Users/silva/Documents/Repositorio/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
                    header = T)
+death_cases <- fread("C:/Users/silva/Documents/Repositorio/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
+                     header = T)
 
-new_cases_date <- fread("C:/Users/silva/Documents/Repositorio/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
+cases_date <- fread("C:/Users/silva/Documents/Repositorio/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
                     header = F)[1,c(-1,-2,-3,-4)]  
+cases_date <- mdy(cases_date)
 
-cases_date <- mdy(new_cases_date)
 
-Brazil <- as.numeric(new_cases %>% filter(`Country/Region` == 'Brazil') %>% select(-`Province/State`, -`Country/Region`,-Lat, -Long))
+Brazil_cases <- as.numeric(new_cases %>% filter(`Country/Region` == 'Brazil') %>% select(-`Province/State`, -`Country/Region`,-Lat, -Long))
 
-Brazil <- Brazil[Brazil > 0]
-########## previsão #########
+Brazil_death_cases <- as.numeric(death_cases %>% filter(`Country/Region` == 'Brazil') %>% select(-`Province/State`, -`Country/Region`,-Lat, -Long))
+
+Brazil_cases_fd <- Brazil_cases[Brazil_cases > 0]
+
+########## new cases per day ########## 
+NCPD <- data.frame(diff(Brazil_cases), (cases_date[-1]))
+colnames(NCPD) <- c("New_cases", "Date")
+
+ggplot(NCPD, aes(x = Date, y = New_cases)) + geom_col(col = "white", fill = "darkblue") + 
+  ggtitle("New cases per day in Brazil") + ylab("New cases") + theme_bw()
+
+#barplot(NCPD$New_cases)
+########## new death cases per day ########## 
+NDCPD <- data.frame(diff(Brazil_death_cases), (cases_date[-1]))
+colnames(NDCPD) <- c("New_death_cases", "Date")
+
+ggplot(NDCPD, aes(x = Date, y = New_death_cases)) + geom_col(col = "white", fill = "black") + 
+  ggtitle("New deaths cases per day in Brazil") + ylab("Deaths per day") + theme_bw()
+
+########## previs�o #########
+########## prediction #########
 #rm(list = ls())
 library (deSolve)
 
@@ -53,7 +74,7 @@ transmission_probability = 0.07       # transmission probability
 infectious_period = 5.55                 # infectious period
 
 #computando os valores de tranmisção e de recuperação
-r_0 <- 2.39
+r_0 <- 2.03 # imperial college r_0 = 2.0 - 2.4 - 2.6
 beta_value = r_0/5.55#contact_rate * transmission_probability
 gamma_value = ((r_0/5.55)/r_0)#1 / infectious_period
 
@@ -81,7 +102,7 @@ output = lsoda (initial_values, timepoints, sis_model, parameter_list)
 
 ###### plot
 # susceptible hosts over time
-plot (I ~ time, data = output, type='l', ylim = c(0,0.1),
+plot (I ~ time, data = output, type='l', ylim = c(0,1),
       #,xlim=c(0,50),
       col = 'red', ylab = 'S, I, S', main = 'SIS epidemic') 
 
@@ -91,9 +112,9 @@ plot (I ~ time, data = output, type='l', ylim = c(0,0.1),
 # infectious hosts over time
 #plot (S ~ time, data = output, type='b', ylim = c(0,1), col = 'blue', ylab = '', axes = FALSE) 
 
-lines(Brazil/210000000)
+lines(Brazil_cases_fd/210000000)
 
-cbind(output[1:length(Brazil),],Brazil/210000000)
+cbind(output[1:length(Brazil_cases_fd),],Brazil_cases_fd/210000000)
 
 # http://desolve.r-forge.r-project.org/
 # https://rpubs.com/choisy/sir
